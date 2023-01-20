8da79677c2b698d0100ff41e818310ae268a42ad
Valentin130493 <gerasimenko130493@gmail.com>
2023-01-20 18:38:46 +0200

    refactor code and fis css

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
     const [openDialog, setOpenDialog] = useState(false)
     const [formValues, setFormValues] = useState({})
     const [error, serError] = useState({})
-    console.log(openDialog)
-    console.log(formData)
+
+
     useEffect(() => {
         setLoading(true)
         axios.get(`https://test-nscu.onrender.com/form`)
@@ -25,8 +25,16 @@ const App = () => {
         setTimeout(() => setLoading(false), 1500)
     }, [])
 
-    const nextHandleClick = () => {
+    useEffect(() => {
+        if (Number(step) !== 0) {
+            setFormValues(prev => ({
+                ...prev,
+                service: formData.body[0].form[formData.body[0].form.length - 1].elements[step - 1].label
+            }))
+        }
+    }, [step])
 
+    const nextHandleClick = () => {
         if (!Object.keys(error).length && Object.keys(formValues).length > 2) {
             const condition = formValues?.service === undefined ? 0 : formValues?.service
             setStep(Number(condition))
@@ -38,6 +46,7 @@ const App = () => {
         setStep(0)
         setFormValues({})
     }
+
     const handleInputChange = (e) => {
         e.target.type === "checkbox" ?
             setFormValues({...formValues, [e.target.name]: `${e.target.checked}`}) :
@@ -48,36 +57,37 @@ const App = () => {
         serError({})
 
     }
+
     const radioButtonClick = (e) => {
+        console.log(e.target.label)
         setFormValues({...formValues, [e.target.name]: e.target.value})
     }
+
     const handleClearForm = (e) => {
         e.preventDefault()
         setFormValues({})
         serError({})
         setStep(0)
     }
+
     const handleSubmit = async (e) => {
         e.preventDefault()
-        setFormValues({
-            ...formValues,
-            service: formData.body[0].form[formData.body[0].form.length - 1].elements[step - 1].label
-        })
         setOpenDialog(true)
-        await axios.post(`https://test-nscu.onrender.com/docs`, formValues)
+        console.log(formValues)
+        // await axios.post(`https://test-nscu.onrender.com/docs`, formValues)
     }
+
     const handleClickAgain = () => {
         setFormValues({})
         setOpenDialog(false)
         setStep(0)
     }
+
     const handleBlur = (e) => {
         if (!e.target.value) {
             const message = "Required"
             serError({...error, [e.target.name]: message})
         }
-
-
     }
 
     return (
@@ -97,16 +107,16 @@ const App = () => {
                                         <div
                                             className={`${(step !== 0 && index1 === 0) ? "form__group__uniq" : "form__group"}`}
                                             key={`${index1}`}>
-                                            <p className="question"> {item.title}</p>
-                                            {item.element.tag === "input" && <input type={`${ item.element.type}`}
-                                                                                    className={` input ${Object.keys(error).includes(item.element.name) ? "error errorInput" : ""}`}
-                                                                                    name={`${item.element.name}`}
-                                                                                    minLength={2}
-                                                                                    value={Object.keys(formValues).includes(item.element.name) ? formValues[item.element.name] : ""}
-                                                                                    onChange={(e) => handleInputChange(e)}
-                                                                                    onBlur={(e) => handleBlur(e)}
-                                                                                    placeholder={`${item.element.placeholder}`}
-                                            />}
+                                            <p className="question"> {item.element.title}</p>
+                                            <input type={`${item.element.type}`}
+                                                   className={` input ${Object.keys(error).includes(item.element.name) ? "error errorInput" : ""}`}
+                                                   name={`${item.element.name}`}
+                                                   minLength={2}
+                                                   value={Object.keys(formValues).includes(item.element.name) ? formValues[item.element.name] : ""}
+                                                   onChange={(e) => handleInputChange(e)}
+                                                   onBlur={(e) => handleBlur(e)}
+                                                   placeholder={`${item.element.placeholder}`}
+                                            />
                                             {error && <p className={"errorMessage"}>{error[item.element.name]}</p>}
 
                                         </div>
@@ -118,7 +128,7 @@ const App = () => {
 
                                             <div className="radioBtn service">
                                                 {item?.elements.map((btn, index) => {
-                                                    return <span key={`${index}`}><label htmlFor={btn.value}>{
+                                                    return <label key={`${index}`} htmlFor={btn.value}>{
                                                         btn.label
                                                     }
                                                         <input className="answer" type={btn.type}
@@ -126,7 +136,8 @@ const App = () => {
                                                                value={btn.value}
                                                                onClick={(e) => radioButtonClick(e)}
                                                         />
-                                    </label></span>
+                                                    </label>
+
                                                 })}
 
                                             </div>
@@ -140,7 +151,8 @@ const App = () => {
                                     {index !== 0 && <button type={"submit"} className="btn">
                                         {formData?.buttons?.submit}
                                     </button>}
-                                    <button className="btn" onClick={(e) => handleClearForm(e)}>{formData?.buttons?.clear}
+                                    <button className="btn"
+                                            onClick={(e) => handleClearForm(e)}>{formData?.buttons?.clear}
                                     </button>
                                 </div>
                             </div>
@@ -158,60 +170,46 @@ const App = () => {
                             </div>
                         </div>
                     </dialog>
+
                     <footer>
                         <div className='footer-mainText'>
                             {formData.footer?.mainText}
                         </div>
                         <div className='footer-mainBlock'>
                             <div className='footer-mainBlock-left'>
-                                <a className='footer-links' target='_blank' href={formData.footer?.termsLink}>Terms Of Use</a>
-                                <a className='footer-links' target='_blank' href={formData.footer?.privacyLink}>Privacy</a>
-                                <a className='footer-links' target='_blank' href={formData.footer?.cookiesLink}>Cookies</a>
+                                <a className='footer-links' target='_blank' rel="noreferrer"
+                                   href={formData.footer?.termsLink}>Terms Of
+                                    Use</a>
+                                <a className='footer-links' target='_blank' rel="noreferrer"
+                                   href={formData.footer?.privacyLink}>Privacy</a>
+                                <a className='footer-links' target='_blank' rel="noreferrer"
+                                   href={formData.footer?.cookiesLink}>Cookies</a>
                             </div>
                             <div className='footer-mainBlock-right'>
-                                <div className='footer-mainBlock-right-icon'><img className='icon-images' src={email}/><a className='footer-links' href={`mailto:${formData.footer?.mailLink}`}>{formData.footer?.mailLink}</a></div>
-                                <div className='footer-mainBlock-right-icon'><img className='icon-images' src={phone}/> <a className='footer-links' href={`tel:${formData.footer?.phoneLink}`}>{formData.footer?.phoneLink}</a></div>
-                                <div className='footer-mainBlock-right-icon'><img className='icon-images' src={address}/> <p className='footer-paragraph'>{formData.footer?.address}</p></div>
-                                <div className='abn'><p className='footer-paragraph'>ABN: </p> <p className='footer-paragraph'>{formData.footer?.abn}</p></div>
+                                <div className='footer-mainBlock-right-icon'>
+                                    <img className='icon-images'
+                                         src={email} alt={"email_icon"}/>
+                                    <a target={"_blank"} rel="noreferrer"
+
+                                       className='footer-links'
+                                       href={`mailto:${formData.footer?.mailLink}`}>{formData.footer?.mailLink}</a>
+                                </div>
+                                <div className='footer-mainBlock-right-icon'>
+                                    <img className='icon-images' src={phone} alt={"phone"}/>
+                                    <a className='footer-links' target={"_blank"} rel="noreferrer"
+                                       href={`tel:${formData.footer?.phoneLink}`}>{formData?.footer?.phoneLink}</a>
+                                </div>
+                                <div className='footer-mainBlock-right-icon'>
+                                    <img className='icon-images' src={address} alt={"convert"}/>
+                                    <p className='footer-paragraph'>{formData.footer?.address}</p></div>
+                                <div className='abn'><p className='footer-paragraph'>ABN: </p> <p
+                                    className='footer-paragraph'>{formData?.footer?.abn}</p></div>
                             </div>
                         </div>
-                        {/*<div>*/}
-                        {/*<p className='layout-footer-copyright'>*/}
-                        {/*© 2022 Xarial Pty Limited. All rights reserved.*/}
-                        {/*</p>*/}
-                        {/*</div>*/}
-                        {/*<div className="layout-footer-contact">*/}
-                        {/*    <table>*/}
-                        {/*        <tbody>*/}
-                        {/*        <tr>*/}
-                        {/*            <td className="contact-img"><img className="icon-image" alt="Contact us via e-mail"/></td>*/}
-                        {/*            <td className="contact-text"><a className="mail-link" href="mailto:info@xarial.com">info@xarial.com</a>*/}
-                        {/*            </td>*/}
-                        {/*        </tr>*/}
-                        {/*        <tr>*/}
-                        {/*            <td className="contact-img"><img className="icon-image" alt="Contact us by phone"/></td>*/}
-                        {/*            <td className="contact-text"><a className="phone-link" href="tel:+61 435 577 927">+61 435 577 927</a>*/}
-                        {/*            </td>*/}
-                        {/*        </tr>*/}
-                        {/*        <tr>*/}
-                        {/*            <td className="contact-img"><img className="icon-image" alt="Mailing address"/></td>*/}
-                        {/*            <td className="contact-text">PO BOX 1163, Dee Why, NSW, 2099</td>*/}
-                        {/*        </tr>*/}
-                        {/*        <tr>*/}
-                        {/*            <td className="contact-img">ABN:</td>*/}
-                        {/*            <td className="contact-text">{formData.footer.abn}</td>*/}
-                        {/*        </tr>*/}
-                        {/*        </tbody>*/}
-                        {/*    </table>*/}
-                        {/*</div>*/}
-                        {/*<div className='layout-footer-links'>*/}
-                        {/*    <span>*/}
-                        {/*        <a className="legal-link" href='/terms-of-us/'> Terms Of Use</a>*/}
-                        {/*        <a className="legal-link" href='/privacy-policy' >Privacy</a>*/}
-                        {/*        <a className="legal-link" href='cookies-policy'>Cookies</a>*/}
-                        {/*    </span>*/}
-                        {/*</div>*/}
+
                     </footer>
+
+
                 </>
             }
         </div>
